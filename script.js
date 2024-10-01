805ae46d07d0d6b1356791682f765fdbd164db3c
vex <113200653+vexthecoder@users.noreply.github.com>
2024-09-30 20:42:17 -0500

    1.1 test 8.2

--- script.js
+++ script.js
@@ -1,43 +1,8 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    let total = parseInt(getCookie('inventoryTotal')) || 0;
+    let total = parseInt(getCookie('total')) || 0;
     const versionNumber = await fetchVersionNumber();
     const noAlertCookie = getCookie('currentversionn');
     const currentUrl = window.location.href;
-    const auras = JSON.parse(getCookie('auras')) || [];
-
-    function updateAurasList() {
-        const aurasList = document.getElementById('aurasList');
-        aurasList.innerHTML = '';
-
-        auras.forEach((aura, index) => {
-            const li = document.createElement('li');
-            li.textContent = aura;
-            li.addEventListener('click', () => removeAura(index));
-            aurasList.appendChild(li);
-        });
-    }
-
-    function removeAura(index) {
-        auras.splice(index, 1);
-        setCookie('auras', JSON.stringify(auras), 365);
-        updateAurasList();
-    }
-
-    const dropdownToggle = document.querySelector('.dropdown-toggle');
-    const dropdownMenu = document.getElementById('dropdownMenu');
-
-    dropdownToggle.addEventListener('click', () => {
-        dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-    });
-
-    window.addEventListener('click', (event) => {
-        if (!event.target.closest('.dropdown')) {
-            dropdownMenu.style.display = 'none';
-        }
-    });
-
-    document.getElementById('total').innerText = formatValue(total);
-    updateAurasList();
 
     async function fetchVersionNumber() {
         try {
@@ -54,7 +19,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         const updateNotice = document.getElementById('updateNotice');
         const versionDisplay = document.getElementById('versionNumber');
 
-        versionDisplay.innerText = versionNumber;
+        if (versionDisplay) {
+            versionDisplay.innerText = versionNumber;
+        }
         updateNotice.style.display = 'block';
 
         document.getElementById('closeNotice').addEventListener('click', () => {
@@ -72,44 +39,59 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     document.querySelector('.settings-open').addEventListener('click', () => {
         const settingsModal = document.querySelector('.settings-modal');
-        settingsModal.style.display = 'flex';
+        if (settingsModal) {
+            settingsModal.style.display = 'flex';
+        }
     });
 
-    document.getElementById('settings-close').addEventListener('click', () => {
-        const settingsModal = document.querySelector('.settings-modal');
-        settingsModal.style.display = 'none';
-    });
+    const settingsCloseButton = document.getElementById('settings-close');
+    if (settingsCloseButton) {
+        settingsCloseButton.addEventListener('click', () => {
+            const settingsModal = document.querySelector('.settings-modal');
+            if (settingsModal) {
+                settingsModal.style.display = 'none';
+            }
+        });
+    }
 
-    document.getElementById('gif-toggle').addEventListener('change', (event) => {
-        const isChecked = event.target.checked;
-        applyGifToggle(isChecked);
-        setCookie('gifToggle', isChecked.toString(), 365);
-    });
+    const gifToggle = document.getElementById('gif-toggle');
+    if (gifToggle) {
+        gifToggle.addEventListener('change', (event) => {
+            const isChecked = event.target.checked;
+            applyGifToggle(isChecked);
+            setCookie('gifToggle', isChecked.toString(), 365);
+        });
+    }
 
-    document.getElementById('theme-select').addEventListener('change', (event) => {
-        const selectedTheme = event.target.value;
-        applyTheme(selectedTheme);
-        setCookie('theme', selectedTheme, 365);
-    });
+    const themeSelect = document.getElementById('theme-select');
+    if (themeSelect) {
+        themeSelect.addEventListener('change', (event) => {
+            const selectedTheme = event.target.value;
+            applyTheme(selectedTheme);
+            setCookie('theme', selectedTheme, 365);
+        });
+    }
 
     const searchInput = document.getElementById('searchInput');
     const gridItems = document.querySelectorAll('.grid-item');
 
-    searchInput.addEventListener('input', () => {
-        const searchValue = searchInput.value.toLowerCase().trim();
+    if (searchInput) {
+        searchInput.addEventListener('input', () => {
+            const searchValue = searchInput.value.toLowerCase().trim();
 
-        gridItems.forEach(item => {
-            const keywords = item.getAttribute('search-terms').toLowerCase().split(' ');
-            const itemLabel = item.querySelector('.image-label').textContent.toLowerCase();
-            const matchesKeyword = keywords.some(keyword => keyword.includes(searchValue));
+            gridItems.forEach(item => {
+                const keywords = item.getAttribute('search-terms').toLowerCase().split(' ');
+                const itemLabel = item.querySelector('.image-label').textContent.toLowerCase();
+                const matchesKeyword = keywords.some(keyword => keyword.includes(searchValue));
 
-            if (itemLabel.includes(searchValue) || matchesKeyword) {
-                item.style.display = 'block';
-            } else {
-                item.style.display = 'none';
-            }
+                if (itemLabel.includes(searchValue) || matchesKeyword) {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
         });
-    });
+    }
 
     const addButtons = document.querySelectorAll('.add-button');
     addButtons.forEach(button => {
@@ -118,12 +100,10 @@ document.addEventListener('DOMContentLoaded', async () => {
             const value = parseInt(input.value) || 0;
             const multiplier = button.closest('.grid-item').getAttribute('data-rarity');
             total += value * multiplier;
-            auras.push(`${value} × ${button.closest('.grid-item').querySelector('.image-label').textContent}`);
-            setCookie('auras', JSON.stringify(auras), 365);
             if (total < 0) total = 0;
             document.getElementById('total').innerText = formatValue(total);
+            setCookie('total', total, 365);
             input.value = '1';
-            updateAurasList();
         });
     });
 
@@ -134,36 +114,39 @@ document.addEventListener('DOMContentLoaded', async () => {
             const value = parseInt(input.value) || 0;
             const multiplier = button.closest('.grid-item').getAttribute('data-rarity');
             total -= value * multiplier;
-            auras.push(`-${value} × ${button.closest('.grid-item').querySelector('.image-label').textContent}`);
-            setCookie('auras', JSON.stringify(auras), 365);
             if (total < 0) total = 0;
             document.getElementById('total').innerText = formatValue(total);
+            setCookie('total', total, 365);
             input.value = '1';
-            updateAurasList();
         });
     });
 
     const clearButton = document.querySelector('.clear-button');
-    clearButton.addEventListener('click', () => {
-        total = 0;
-        document.getElementById('total').innerText = total;
-    });
+    if (clearButton) {
+        clearButton.addEventListener('click', () => {
+            total = 0;
+            document.getElementById('total').innerText = total;
+            setCookie('total', total, 365);
+        });
+    }
 
     function formatValue(number) {
         return number.toLocaleString('en-US');
     }
 
     const copyButton = document.querySelector('.copy-button');
-    copyButton.addEventListener('click', () => {
-        const totalValue = document.getElementById('total').innerText;
-        navigator.clipboard.writeText("My Sol's RNG inventory value is " + totalValue + "! What's yours?\nCalculate your inventory value at https://bit.ly/rng-calculator and share it to friends!")
-            .then(() => {
-                console.log('Total value copied to clipboard:', totalValue);
-            })
-            .catch(error => {
-                console.error('Failed to copy text: ', error);
-            });
-    });
+    if (copyButton) {
+        copyButton.addEventListener('click', () => {
+            const totalValue = document.getElementById('total').innerText;
+            navigator.clipboard.writeText("My Sol's RNG inventory value is " + totalValue + "! What's yours?\nCalculate your inventory value at https://bit.ly/rng-calculator and share it to friends!")
+                .then(() => {
+                    console.log('Total value copied to clipboard:', totalValue);
+                })
+                .catch(error => {
+                    console.error('Failed to copy text: ', error);
+                });
+        });
+    }
 });
 
 function setCookie(name, value, days) {
@@ -174,14 +157,19 @@ function setCookie(name, value, days) {
 }
 
 function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
+    const cname = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
     for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
+        }
     }
-    return null;
+    return "";
 }
 
 function applyTheme(theme) {
@@ -194,13 +182,18 @@ function applyTheme(theme) {
     }
 }
 
-function applyGifToggle(isChecked) {
-    const gifContainer = document.querySelector('.gif-container');
-    if (isChecked) {
-        gifContainer.style.display = 'block';
-    } else {
-        gifContainer.style.display = 'none';
-    }
+function applyGifToggle(gifToggle) {
+    const gridItems = document.querySelectorAll('.grid-item img');
+
+    gridItems.forEach(img => {
+        const imgSrc = img.getAttribute('data-img-src');
+        const gifSrc = img.getAttribute('data-gif-src');
+        if (gifToggle) {
+            img.src = gifSrc;
+        } else {
+            img.src = imgSrc;
+        }
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
