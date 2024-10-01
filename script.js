1569d377c4d4d0e5c7cc81747a39a55016bb00cc
vex <113200653+vexthecoder@users.noreply.github.com>
2024-09-30 21:27:05 -0500

    revert to 1.1 test 7.2

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    let total = 0;
-    let auraList = JSON.parse(localStorage.getItem('auraList')) || [];
-
-    const versionNumber = await fetchVersionNumber();
-    const savedVersion = localStorage.getItem('currentversionn') || '';
-    const currentUrl = window.location.href;
-    const dropdownIcon = document.getElementById('dropdown-icon');
-    const auraDropdown = document.getElementById('auraDropdown');
-    const auraListContainer = document.getElementById('auraListContainer');
-    const totalContainer = document.querySelector('.total-container');
-    const totalSpan = document.getElementById('total');
-    const copyButton = document.querySelector('.copy-button');
-    const clearButton = document.querySelector('.clear-button');
-
-    if (!currentUrl.includes("/index.html") && savedVersion !== versionNumber) {
-        const updateNotice = document.getElementById('updateNotice');
-        const versionDisplay = document.getElementById('versionNumber');
-        versionDisplay.innerText = versionNumber;
-        updateNotice.style.display = 'block';
-        document.getElementById('closeNotice').addEventListener('click', () => {
-            updateNotice.style.display = 'none';
-            localStorage.setItem('currentversionn', versionNumber);
-        });
-    }
+    let total = localStorage.getItem('inventoryTotal') ? parseInt(localStorage.getItem('inventoryTotal')) : 0;
+    document.getElementById('total').innerText = formatValue(total);
 
     const savedGifToggle = localStorage.getItem('gifToggle') === 'true';
     const savedTheme = localStorage.getItem('theme') || 'dark';
+    const currentVersionNumber = await fetchVersionNumber();
+    const savedVersionNumber = localStorage.getItem('currentVersionNumber');
+    const currentUrl = window.location.href;
+
+    async function fetchVersionNumber() {
+        try {
+            const response = await fetch('version');
+            const versionText = await response.text();
+            return versionText.trim();
+        } catch (error) {
+            console.error('Error fetching version number:', error);
+            return 'unknown';
+        }
+    }
+
+    if (!currentUrl.includes("/index.html") && savedVersionNumber !== currentVersionNumber) {
+        const updateNotice = document.getElementById('updateNotice');
+        const versionDisplay = document.getElementById('versionNumber');
+
+        versionDisplay.innerText = currentVersionNumber;
+        updateNotice.style.display = 'block';
+
+        document.getElementById('closeNotice').addEventListener('click', () => {
+            updateNotice.style.display = 'none';
+            localStorage.setItem('currentVersionNumber', currentVersionNumber);
+        });
+    }
+
     applyGifToggle(savedGifToggle);
     applyTheme(savedTheme);
     document.getElementById('gif-toggle').checked = savedGifToggle;
@@ -53,43 +59,23 @@ document.addEventListener('DOMContentLoaded', async () => {
         localStorage.setItem('theme', selectedTheme);
     });
 
-    dropdownIcon.addEventListener('click', () => {
-        auraDropdown.classList.toggle('open');
-        totalContainer.classList.toggle('shifted-up');
-        renderAuraList();
-    });
+    const searchInput = document.getElementById('searchInput');
+    const gridItems = document.querySelectorAll('.grid-item');
 
-    function renderAuraList() {
-        auraListContainer.innerHTML = '';
-        auraList.forEach(aura => {
-            const auraItem = document.createElement('div');
-            auraItem.classList.add('aura-item');
-            auraItem.innerHTML = `
-                <span class="aura-name">${aura.name}</span>
-                <span class="aura-quantity">x${aura.quantity}</span>
-                <span class="aura-rarity">Rarity: ${aura.rarity}</span>
-                <button class="remove-aura">Remove 1</button>
-            `;
-            auraItem.querySelector('.remove-aura').addEventListener('click', () => {
-                removeAura(aura.name);
-            });
-            auraListContainer.appendChild(auraItem);
-        });
-    }
+    searchInput.addEventListener('input', () => {
+        const searchValue = searchInput.value.toLowerCase().trim();
+        gridItems.forEach(item => {
+            const keywords = item.getAttribute('search-terms').toLowerCase().split(' ');
+            const itemLabel = item.querySelector('.image-label').textContent.toLowerCase();
+            const matchesKeyword = keywords.some(keyword => keyword.includes(searchValue));
 
-    function removeAura(auraName) {
-        const auraIndex = auraList.findIndex(aura => aura.name === auraName);
-        if (auraIndex !== -1) {
-            if (auraList[auraIndex].quantity > 1) {
-                auraList[auraIndex].quantity--;
+            if (itemLabel.includes(searchValue) || matchesKeyword) {
+                item.style.display = 'block';
             } else {
-                auraList.splice(auraIndex, 1);
+                item.style.display = 'none';
             }
-            localStorage.setItem('auraList', JSON.stringify(auraList));
-            renderAuraList();
-            updateTotal();
-        }
-    }
+        });
+    });
 
     const addButtons = document.querySelectorAll('.add-button');
     addButtons.forEach(button => {
@@ -97,21 +83,11 @@ document.addEventListener('DOMContentLoaded', async () => {
             const input = button.previousElementSibling;
             const value = parseInt(input.value) || 0;
             const multiplier = button.closest('.grid-item').getAttribute('data-rarity');
-            const auraName = button.closest('.grid-item').querySelector('.aura-name').innerText;
-
-            const aura = auraList.find(aura => aura.name === auraName);
-            if (aura) {
-                aura.quantity += value;
-            } else {
-                auraList.push({ name: auraName, quantity: value, rarity: multiplier });
-            }
-
             total += value * multiplier;
-            total = Math.max(total, 0);
-            localStorage.setItem('auraList', JSON.stringify(auraList));
-            localStorage.setItem('totalValue', total);
-            totalSpan.innerText = formatValue(total);
+            if (total < 0) total = 0;
+            document.getElementById('total').innerText = formatValue(total);
             input.value = '1';
+            localStorage.setItem('inventoryTotal', total);
         });
     });
 
@@ -121,65 +97,87 @@ document.addEventListener('DOMContentLoaded', async () => {
             const input = button.previousElementSibling.previousElementSibling;
             const value = parseInt(input.value) || 0;
             const multiplier = button.closest('.grid-item').getAttribute('data-rarity');
-            const auraName = button.closest('.grid-item').querySelector('.aura-name').innerText;
-
-            const aura = auraList.find(aura => aura.name === auraName);
-            if (aura) {
-                aura.quantity = Math.max(0, aura.quantity - value);
-                if (aura.quantity === 0) {
-                    auraList = auraList.filter(a => a.name !== auraName);
-                }
-            }
-
             total -= value * multiplier;
-            total = Math.max(total, 0);
-            localStorage.setItem('auraList', JSON.stringify(auraList));
-            localStorage.setItem('totalValue', total);
-            totalSpan.innerText = formatValue(total);
+            if (total < 0) total = 0;
+            document.getElementById('total').innerText = formatValue(total);
             input.value = '1';
+            localStorage.setItem('inventoryTotal', total);
         });
     });
 
+    const clearButton = document.querySelector('.clear-button');
     clearButton.addEventListener('click', () => {
         total = 0;
-        auraList = [];
-        localStorage.setItem('auraList', JSON.stringify(auraList));
-        localStorage.setItem('totalValue', total);
-        totalSpan.innerText = formatValue(total);
-        renderAuraList();
+        document.getElementById('total').innerText = total;
+        localStorage.setItem('inventoryTotal', total);
     });
-
-    function updateTotal() {
-        total = auraList.reduce((acc, aura) => acc + aura.quantity * aura.rarity, 0);
-        localStorage.setItem('totalValue', total);
-        totalSpan.innerText = formatValue(total);
-    }
 
     function formatValue(number) {
         return number.toLocaleString('en-US');
     }
 
+    const copyButton = document.querySelector('.copy-button');
     copyButton.addEventListener('click', () => {
-        const totalValue = totalSpan.innerText;
-        navigator.clipboard.writeText(`My Sol's RNG inventory value is ${totalValue}! What's yours?\nCalculate your inventory value at https://bit.ly/rng-calculator and share it with friends!`);
+        const totalValue = document.getElementById('total').innerText;
+        navigator.clipboard.writeText("My Sol's RNG inventory value is " + totalValue + "! What's yours?\nCalculate your inventory value at https://bit.ly/rng-calculator and share it to friends!")
+            .then(() => {
+                console.log('Total value copied to clipboard:', totalValue);
+            })
+            .catch(error => {
+                console.error('Failed to copy text: ', error);
+            });
     });
+});
 
-    total = parseInt(localStorage.getItem('totalValue')) || 0;
-    totalSpan.innerText = formatValue(total);
-    renderAuraList();
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
 
-    function setLocalStorage(key, value) {
-        localStorage.setItem(key, value);
-    }
-
-    async function fetchVersionNumber() {
-        try {
-            const response = await fetch('version');
-            const versionText = await response.text();
-            return versionText.trim();
-        } catch (error) {
-            console.error('Error fetching version number:', error);
-            return 'unknown';
+function getCookie(name) {
+    const cname = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
         }
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+function applyTheme(theme) {
+    if (theme === 'dark') {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+    } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+    }
+}
+
+function applyGifToggle(gifToggle) {
+    const gridItems = document.querySelectorAll('.grid-item img');
+    gridItems.forEach(img => {
+        const imgSrc = img.getAttribute('data-img-src');
+        const gifSrc = img.getAttribute('data-gif-src');
+        if (gifToggle) {
+            img.src = gifSrc;
+        } else {
+            img.src = imgSrc;
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentUrl = window.location.href;
+    if (currentUrl.includes("/index.html")) {
+        window.location.replace(currentUrl.replace("/index.html", ""));
     }
 });
